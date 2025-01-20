4acac855ae607958d9f2c708093fd41af7d0ca2a
AzizManiyazov <dj.azik93@gmail.com>
2025-01-20 22:26:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let num1 = prompt("Число 1");
+let oper = prompt("+, -, *, /");
+let num2 = prompt("Число 2");
+
+if (oper === "+") {
+    result = num1 + num2;
+} else if (oper === "-") {
+    result = num1 - num2;
+} else if (oper === "*") {
+    result = num1 * num2;
+} else if (oper === "/") {
+    if (num2 !== 0) {
+        result = num1 / num2;
+    } else {
+        result = "Ошибка!";
+    }
+}
+alert("Ответ!" + result);
